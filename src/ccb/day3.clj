8c0e79047b12f95187156180291da0942f0cc735
Cesar Castillo <ccastill100@gmail.com>
2022-12-07 15:19:01 +0100

    Day 3 part 1 done

---
+++ src/ccb/day3.clj
@@ -0,0 +1,42 @@
+(ns day3.clj
+  (:require [clojure.java.io :as io]
+            [clojure.set :as set]
+            ;;[clojure.string :as str]
+            ))
+
+(comment
+  (def filename "day3-ex.txt")
+  ;; for priority
+  (defn build-priorities
+    [start-char start-offset]
+    (mapcat
+     (fn [offset]
+       [(char (+ start-char offset)) (+ 1 offset start-offset)])
+     (range 0 26)))
+  (def priorities 
+    (apply (partial assoc {}) 
+           (concat 
+            (build-priorities 65 26)
+            (build-priorities 97 0))))
+  
+  (def s (first lines))
+  (defn find-wrong-item [s]
+    (let [s-len (count s)
+          first-half (subs s 0 (/ s-len 2))
+          second-half (subs s (/ s-len 2) s-len)]
+      (first (set/intersection (set first-half) (set second-half)))))
+  (defn day-3-part-1
+    [filename]
+    (let [lines
+          (line-seq
+           (io/reader
+            (io/resource filename)))]
+      (->> lines
+           (map find-wrong-item)
+           (map priorities)
+           (reduce + 0))))
+
+  (day-3-part-1 filename);; 157
+  (day-3-part-1 "day3.input.txt") ;; 7872
+
+  ,)
