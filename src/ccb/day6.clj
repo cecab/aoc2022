97d381ff0210b81c57ba82b7f568ff4ff2c84f77
Cesar Castillo <ccastill100@gmail.com>
2022-12-13 09:01:32 +0100

    Day 6 part 2 done - Thanks to peers in Clojure Berlin meetup

--- src/ccb/day6.clj
+++ src/ccb/day6.clj
@@ -1,4 +1,5 @@
-(ns ccb.day6)
+(ns ccb.day6
+  (:require [clojure.java.io :as io]))
 (defn day-6-part-1 [line]
     (loop [
            solution 0
@@ -9,16 +10,26 @@
           :else
           (recur (inc solution)
                  (rest input))))))
+(defn day-6-part-1-partition [filename]
+  (let [line (slurp
+              (io/resource filename))]
+    (+ 14
+       (count
+        (take-while #(not= 14 (count (set %)))
+                    (partition 14 1 line))))))
 
 (comment
+  (f1)
+  (def filename "day6.input.txt")
   
 
-  (day-6-part-1 "bvwbjplbgvbhsrlpgdmjqwftvncz") ;; 5
-  (day-6-part-1 "nppdvjthqldpwncqszvftbrmjlhg") ;; 6
-  (day-6-part-1 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") ;; 10
-  (day-6-part-1 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") ;; 11
+  (day-6-part-1-partition "bvwbjplbgvbhsrlpgdmjqwftvncz")      ;; 5
+  (day-6-part-1-partition "nppdvjthqldpwncqszvftbrmjlhg")      ;; 6
+  (day-6-part-1-partition "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") ;; 10
+  (day-6-part-1-partition "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")  ;; 11
   
   (def line
     (slurp "resources/day6-ex.txt"))
   "mjqjpqmgbljsphdztnvjfqwrcgsmlb\n"
+  (day-6-part-1-partition "day6.input.txt") ;; 3837
   ,)
