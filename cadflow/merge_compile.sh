0b897b71fb60df0ba20f390ac27f77c98fbb26ee
Cesar Castillo <ccastill100@gmail.com>
2024-02-08 12:10:19 +0100

    Doing it... with numbers

--- cadflow/merge_compile.sh
+++ cadflow/merge_compile.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -x
 issues="$*"
+branches=""
 for issue in $issues ; do
     echo "Fetching and merging.. issue  $issue"
+    issue_branch=`git branch -a | grep -- "-issue-${issue}-" | tr -d " "`
+    branches="${branches} $issue_branch"
 done
+echo "Mering branches: $branches"
 exit 0
