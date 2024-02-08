a5d176b8aafcaf0c076d083a5e1a5be85b723ccb
Cesar Castillo <ccastill100@gmail.com>
2024-02-08 14:22:31 +0100

    checkout first into main branch

--- cadflow/merge_compile.sh
+++ cadflow/merge_compile.sh
@@ -7,7 +7,10 @@ for issue in $issues ; do
     issue_branch=`git branch -a | grep -- "-issue-${issue}-" | tr -d " "`
     branches="${branches} $issue_branch"
 done
-echo "Mering branches: $branches"
+echo "into main..."
+git checkout -b cadflow/deploy origin/main
+git status
+echo "Merging branches: $branches "
 git merge $branches
 find . | grep -v \.git
 exit 0
